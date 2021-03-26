879d74e8e994892474380d20b14a41eb732725a5
nolwn <nolan.hellyer@gmail.com>
2021-03-25 20:55:36 -0700

    Lesson 09: Viewport

--- main.go
+++ main.go
@@ -3,31 +3,39 @@ package main
 import (
 	"fmt"
 
+	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-// Screen dimension constants
+//Screen dimension constants
 const screenWidth = 640
 const screenHeight = 480
 
 //The window we'll be rendering to
 var gWindow *sdl.Window
 
-//The surface contained by the window
-var gScreenSurface *sdl.Surface
+//The window renderer
+var gRenderer *sdl.Renderer
 
-//The image we will load and show on the screen
-var gHelloWorld *sdl.Surface
+//Current displayed texture
+var gTexture *sdl.Texture
 
 func init() {
 	var err error
 
 	//Initialize SDL
-	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		fmt.Printf("SDL could not initialize! SDL_Error: %s\n", err)
+	if err = sdl.Init(sdl.INIT_VIDEO); err != nil {
+		fmt.Printf("SDL could not initialize! SDL Error: %s\n", err)
 		panic(err)
 	}
 
+	if enabled := sdl.SetHint(
+		sdl.HINT_RENDER_SCALE_QUALITY,
+		"1"); !enabled { //Set texture filtering to linear
+		fmt.Printf("Warning: Linear texture filtering not enabled!\n")
+	}
+
+	//Create window
 	if gWindow, err = sdl.CreateWindow(
 		"SDL Tutorial",
 		sdl.WINDOWPOS_UNDEFINED,
@@ -35,63 +43,138 @@ func init() {
 		screenWidth,
 		screenHeight,
 		sdl.WINDOW_SHOWN); err != nil {
-		fmt.Printf("SDL could not initialize! SDL_Error: %s\n", err)
-
+		fmt.Printf("Window could not be created! SDL Error: %s\n", err)
 		panic(err)
 	}
 
-	if gScreenSurface, err = gWindow.GetSurface(); err != nil {
-		fmt.Printf("SDL could not initialize! SDL_Error: %s\n", err)
-
+	//Create renderer for window
+	if gRenderer, err = sdl.CreateRenderer(gWindow, -1, sdl.RENDERER_ACCELERATED); err != nil {
+		fmt.Printf("Renderer could not be created! SDL Error: %s\n", err)
 		panic(err)
+	}
+
+	//Initialize renderer color
+	gRenderer.SetDrawColor(0xFF, 0xFF, 0xFF, 0xFF)
+
+	//Initialize PNG loading
+	if err := img.Init(img.INIT_PNG); err != nil {
+		fmt.Printf("SDL_image could not initialize! SDL_image Error: %s\n", err)
 	}
 }
 
-func loadMedia() {
-	var err error
+func loadMedia() (err error) {
+	//Load texture
 
-	//Load splash image
-	gHelloWorld, err = sdl.LoadBMP("media/hello_world.bmp")
-	if err != nil {
-		fmt.Printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", err)
+	if gTexture = loadTexture("media/viewport.png"); err != nil {
+		fmt.Printf("Failed to load texture image!\n")
 	}
+
+	return
 }
 
 func close() {
-	//Deallocate surface
-	gHelloWorld.Free()
+	//Free loaded image
+	gTexture.Destroy()
 
 	//Destroy window
+	gRenderer.Destroy()
 	gWindow.Destroy()
 
 	//Quit SDL subsystems
+	img.Quit()
 	sdl.Quit()
 }
 
+func loadTexture(path string) (newTexture *sdl.Texture) {
+	var err error
+
+	//Load image at specified path
+	loadedSurface, err := img.Load(path)
+
+	if err != nil {
+		fmt.Printf("Unable to load image %s! SDL_image Error: %s\n", path, err)
+	}
+	//Create texture from surface pixels
+	if newTexture, err = gRenderer.CreateTextureFromSurface(loadedSurface); err != nil {
+		fmt.Printf("Unable to create texture from %s! SDL Error: %s\n", path, err)
+	}
+
+	//Get rid of old loaded surface
+	loadedSurface.Free()
+
+	return
+}
+
 func main() {
-	defer close()
-
 	//Load media
-	loadMedia()
+	if err := loadMedia(); err != nil {
+		fmt.Printf("Failed to load media!\n")
+	} else {
+		//Main loop flag
+		quit := false
 
-	running := true
+		//While application is running
+		for !quit {
+			//Event handler
+			e := sdl.PollEvent()
 
-	for running {
-		e := sdl.PollEvent()
+			//Handle events on queue
+			for e != nil {
+				//User requests quit
+				if e.GetType() == sdl.QUIT {
+					quit = true
+				}
 
-		for e != nil {
-			fmt.Printf("%v", e)
-			if e.GetType() == sdl.QUIT {
-				running = false
+				e = sdl.PollEvent()
 			}
 
-			e = sdl.PollEvent()
+			//Clear screen
+			gRenderer.SetDrawColor(0x00, 0xFF, 0xFF, 0xFF)
+			gRenderer.Clear()
+
+			//Top left corner viewport
+			topLeftViewport := sdl.Rect{
+				X: 0,
+				Y: 0,
+				W: screenWidth,
+				H: screenHeight,
+			}
+
+			gRenderer.Copy(gTexture, nil, nil)
+			gRenderer.SetViewport(&topLeftViewport)
+
+			//Render texture to screen
+			gRenderer.Copy(gTexture, nil, nil)
+
+			//Top right viewport
+			topRightViewport := sdl.Rect{
+				X: screenWidth / 2,
+				Y: 0,
+				W: screenWidth / 2,
+				H: screenHeight / 2,
+			}
+			gRenderer.SetViewport(&topRightViewport)
+
+			//Render texture to screen
+			gRenderer.Copy(gTexture, nil, nil)
+
+			// //Bottom viewport
+			// bottomViewport := sdl.Rect{
+			// 	X: 0,
+			// 	Y: screenHeight / 2,
+			// 	W: screenWidth,
+			// 	H: screenHeight / 2,
+			// }
+			// gRenderer.SetViewport(&bottomViewport)
+
+			// //Render texture to screen
+			// gRenderer.Copy(gTexture, nil, nil)
+
+			//Update screen
+			gRenderer.Present()
 		}
-
-		//Apply the image
-		gHelloWorld.Blit(nil, gScreenSurface, nil)
-
-		//Update the surface
-		gWindow.UpdateSurface()
 	}
+
+	//Free resources and close SDL
+	close()
 }
